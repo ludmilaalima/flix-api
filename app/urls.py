077ff1c6a7611004f85b55338fd4ef226fd6c18b
ludmilaalima <ludmilalima15z@gmail.com>
2025-08-06 23:57:00 -0300

    continue

--- app/urls.py
+++ app/urls.py
@@ -17,10 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app.views import hello_world
-from genres.views import genre_list_view
+from genres.views import genre_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('teste', hello_world),
-    path('genres/', genre_list_view)
+    path('genres/', genre_view)
 ]
